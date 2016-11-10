6b8a58a503ba01e68fa263c4076006e8762a7fa8
Junhao Xie <junhaox.k@gmail.com>
2016-11-09 17:00:26 -0800

    Added UIPickerViews

--- SpecApp/PhoneViewController.swift
+++ SpecApp/PhoneViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class PhoneViewController: UIViewController {
+class PhoneViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var brand: UITextField!
+    @IBOutlet weak var model: UITextField!
+    
+    var brandData = ["Apple", "Google", "Samsung"]
+    var modelData = ["iPhone 7", "iPhone 7 Plus", "Pixel", "Pixel XL", "Galaxy S7 Edge", "Galaxy Note 7"]
+    var brandPicker = UIPickerView()
+    var modelPicker = UIPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        brandPicker.delegate = self
+        brandPicker.dataSource = self
+        modelPicker.delegate = self
+        modelPicker.dataSource = self
+        brand.inputView = brandPicker
+        model.inputView = modelPicker
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,39 @@ class PhoneViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // returns the number of 'columns' to display.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    // returns the # of rows in each component..
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (pickerView == brandPicker) {
+            return brandData.count
+        }
+        else {
+            return modelData.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if (pickerView == brandPicker) {
+            return brandData[row]
+        }
+        else {
+            return modelData[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (pickerView == brandPicker) {
+            brand.text = brandData[row]
+        }
+        else {
+            model.text = modelData[row]
+        }
+    }
 
 }
 
